c1137d91b6567dd328d41a88d9d3f72cea8e9adb
Sam Burke <sburke@unca.edu>
2019-02-05 17:27:08 -0500

    First commit

---
+++ Assets/globalVariables.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class globalVariables
+{
+    public static int units, chefs;
+
+    public static int Units
+    {
+        get
+        {
+            return units;
+         }
+        set
+        {
+            units = value;
+        }
+    }
+
+    public static int Chefs
+    {
+        get
+        {
+            return chefs;
+        }
+        set
+        {
+            chefs = value;
+        }
+    }
+
+}
---
+++ Assets/ownedBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ownedBehavior : MonoBehaviour
+{
+    private Text ownedText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ownedText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      ownedText.text =  "Chefs Owned: " + globalVariables.chefs;
+    }
+
+    public void ChangeUnits(int amt)
+    {
+        if (globalVariables.units - 10 >= 0)
+        {
+            globalVariables.chefs += amt;
+            globalVariables.units -= 10;
+        }
+    }
+}
